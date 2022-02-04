4ab8645f62ff9ffcc5f6f98450f9f9fe1743069f
kongkdh <kongkdh@kong-tech.com>
2022-02-04 16:40:26 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/chatprac/ChatpracApplication.kt
@@ -0,0 +1,11 @@
+package com.example.chatprac
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChatpracApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChatpracApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/chatprac/ChatpracApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.chatprac
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatpracApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
